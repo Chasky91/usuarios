e128d2578586f0cd6a15266456b3fc123dc726f5
David <david_c91@outlook.com>
2026-05-05 17:27:06 -0300

    remoto

--- db/config.db.js
+++ db/config.db.js
@@ -5,7 +5,7 @@ const  pool  = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: process.env.MYSQL_DATABASE || '',
     port: process.env.DB_PORT || 5306,
     waitForConnections: true,
     connectionLimit: 10,
