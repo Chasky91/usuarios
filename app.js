7e2e66ac93c4a7532b6ded130f908710be78a4aa
Chasky91 <david_c91@outlook.com>
2026-05-05 18:08:46 -0300

    cors

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import usuariosRoutes from './routes/usuariosRoutes.js'
-
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 const PORT = 5000
 
